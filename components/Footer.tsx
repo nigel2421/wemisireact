0b29ea6edd63f1f655332f29431fb2b2b62e25dc
nigel2421 <nigel2421@yahoo.com>
2025-11-10 11:38:55 +0300

    name change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-white">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 text-center text-stone-500">
-        <p>&copy; {new Date().getFullYear()} Interior Lux. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Wemisi. All rights reserved.</p>
       </div>
     </footer>
   );
